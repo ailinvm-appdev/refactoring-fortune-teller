c1b0eecdaf52f56f3e941af307b28f84d451531c
ailinvm <ailinvm0@chicagobooth.edu>
2022-07-12 01:54:10 +0000

    final 100

--- config/routes.rb
+++ config/routes.rb
@@ -5,29 +5,30 @@ Rails.application.routes.draw do
   get("/", { :controller => "lottery", :action => "lucky"})
   get("/lottery/lucky", { :controller => "lottery", :action => "lucky"})
   get("/lottery/unlucky", { :controller => "lottery", :action => "unlucky"})
-
+  #get("/roll/:zebra/:giraffe", { :controller => "dice", :action => "two_six" })
+  get("/roll/:number_of_dice/:how_many_sides", { :controller => "dice", :action => "infinity_and_beyond" })
   # PART 2: RCAV DEBUGGING
   # ======================
-
-  get("/zodiacs/aries", { :controller =>  "fire", :action =>  "ram" })
-  get("/zodiacs/leo", { :controller => "fire", :action => "lion" })
-  get("/zodiacs/sagittarius", { :controller => "fire", :action => "archer" })
+  get("/zodiacs/:the_sign", { :controller => "fortunes", :action => "horoscopes" }) 
+  # get("/zodiacs/aries", { :controller =>  "fire", :action =>  "ram" })
+  # get("/zodiacs/leo", { :controller => "fire", :action => "lion" })
+  # get("/zodiacs/sagittarius", { :controller => "fire", :action => "archer" })
   
-  get("/zodiacs/taurus", { :controller => "earth", :action => "bull" })
-  get("/zodiacs/virgo", { :controller => "earth", :action => "maiden" })
-  get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
+  # get("/zodiacs/taurus", { :controller => "earth", :action => "bull" })
+  # get("/zodiacs/virgo", { :controller => "earth", :action => "maiden" })
+  # get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
   
-  get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
-  get("/zodiacs/libra", { :controller => "air", :action => "scales" })
-  get("/zodiacs/aquarius", { :controller => "air", :action => "waterbearer" })
+  # get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
+  # get("/zodiacs/libra", { :controller => "air", :action => "scales" })
+  # get("/zodiacs/aquarius", { :controller => "air", :action => "waterbearer" })
   
-  get("/zodiacs/cancer", { :controller => "water", :action => "crab" })
-  get("/zodiacs/scorpio", { :controller => "water", :action => "scorpion" })
-  get("/zodiacs/pisces", { :controller => "water", :action => "fish" })
+  # get("/zodiacs/cancer", { :controller => "water", :action => "crab" })
+  # get("/zodiacs/scorpio", { :controller => "water", :action => "scorpion" })
+  # get("/zodiacs/pisces", { :controller => "water", :action => "fish" })
 
   # PART 3: MORE R→C→A→V PRACTICE
   # ==========================
-    
+  
   get("/roll/1/6", { :controller => "dice", :action => "one_six" })
   get("/roll/2/6", { :controller => "dice", :action => "two_six" })
   get("/roll/3/6", { :controller => "dice", :action => "three_six" })
